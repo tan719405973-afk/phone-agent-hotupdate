c1f35cf5c686b00766b2a9ae7d604c8f0187ecd0
tan719405973-afk <tan719405973@gmail.com>
2026-08-02 22:50:09 +0800

    v0.4.57 web hotfix: 修复自主模式 FILE_LIST/MKDIR/DELETE 解析缺失 + 生图 agnes-image-2.1-flash response_format 错误

--- test/version.js
+++ test/version.js
@@ -1,5 +1,5 @@
 (function(){
   window.__PA_APPHtml__ = null;
   window.__PA_VERSIONS__ = ["0.4.56", "0.4.55", "0.4.54", "0.4.53", "0.4.52"];
-  window.__PA_APP_VERSION__ = "0.4.56";
+  window.__PA_APP_VERSION__ = "0.4.57";
 })();
